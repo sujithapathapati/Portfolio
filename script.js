66154e363d48f9332ee3a667aba08ac7194d18ff
sujithapathapati <sujithapathapati678@gmail.com>
2026-06-05 13:53:11 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+new Typed("#typing", {
+
+    strings: [
+        "Full Stack Developer",
+        "AI & ML Enthusiast",
+        "Java Developer"
+    ],
+
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+
+});
+
+AOS.init({
+    duration: 1000
+});
